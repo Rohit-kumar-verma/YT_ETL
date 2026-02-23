d2d2beb7af4bed7ebd3ea82aa0f32e97eb39dd02
Rohit Kumar Verma <rohit.kumar.verma@ibm.com>
2026-02-23 22:47:28 +0530

    create extract video data

--- video_stats.py
+++ video_stats.py
@@ -41,12 +41,44 @@ def get_videoIds(Channel_playlistId):
                 pageToken = data.get("nextPageToken")
                 if not pageToken:
                     break
-
+            print(video_ids[0])
             return video_ids
 
         except requests.exceptions.RequestException as e:
             raise e
 
+
+def extract_video_data(video_ids, batch_size):
+    video_details = []
+    for i in range(0, len(video_ids), batch_size):
+        batch_ids = video_ids[i:i + batch_size]
+        
+        url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={','.join(batch_ids)}&key={api_key}"
+        try:
+            response = requests.get(url)
+            data = response.json()
+            for item in data.get("items", []):
+                video_id= item["id"]
+                title = item["snippet"]["title"]
+                duration = item["contentDetails"]["duration"]
+                published_at = item["snippet"]["publishedAt"]
+                view_count = item["statistics"].get("viewCount", None)
+                like_count = item["statistics"].get("likeCount", None)
+                comment_count = item["statistics"].get("commentCount", None)
+                video_details.append({
+                    "video_id": video_id,
+                    "title": title,
+                    "duration": duration,
+                    "publishedAt": published_at,
+                    "view_count": view_count,
+                    "like_count": like_count,
+                    "comment_count": comment_count
+                })
+        except requests.exceptions.RequestException as e:
+            raise e
+    return video_details
+
 if __name__ == "__main__":
     Channel_playlistId = get_video_stats(Channel_handle)
-    get_videoIds(Channel_playlistId)
+    video_ids = get_videoIds(Channel_playlistId)
+    extract_video_data(video_ids, batch_size=50)
